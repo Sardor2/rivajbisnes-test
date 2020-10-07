16bc008acd53d49772814a7878fac5d7188e1406
unknown <abdurakhimov.sardor@gmail.com>
2020-10-07 11:51:53 +0500

    no change

--- src/redux/selectors/cartSelector.js
+++ src/redux/selectors/cartSelector.js
@@ -19,10 +19,6 @@ export const totalSelector = createSelector(
 );
 // ??? 
 export const quantitySelector = (book) => createSelector(
-<<<<<<< HEAD
-  cart, 
-  (cart) => (cart[book.id] ? cart[book.id].quantity : 1)
-=======
   cartItems,
   // (items) => items.filter(item => item.id === book.id)[0].quantity
   items => {
@@ -31,19 +27,10 @@ export const quantitySelector = (book) => createSelector(
     }
     return 1;
   }
->>>>>>> cartExperiment
 );
 
 
 export const totalQuantitySelector = createSelector(
   cartItems,
-<<<<<<< HEAD
-  (items) => items.reduce((prev,curr) => {
-    
-    console.log(prev,curr);
-    return prev + curr.quantity
-  },0)
-=======
   (items) => items.length
->>>>>>> cartExperiment
 );
